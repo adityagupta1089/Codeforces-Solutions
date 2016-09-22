29a80ce8269ce161c286c7e334d9e8f227f9e4f3
Aditya Gupta <adityagupta1089@gmail.com>
2016-09-22 23:08:32 +0530

    .

---
+++ P56B.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P56B {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class Task {
+        public void solve(InputReader in, PrintWriter out) {
+            int n = in.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = in.nextInt();
+            int l = 0, r = n - 1;
+            while (l < n && a[l] == l + 1)
+                l++;
+            while (r >= 0 && a[r] == r + 1)
+                r--;
+            int[] t = Arrays.copyOf(a, a.length);
+            if (l > r) {
+                out.println("0 0");
+                return;
+            }
+            for (int k = l; k <= r; k++) {
+                t[r + l - k] = a[k];
+            }
+            for (int i = 0; i < n; i++)
+                if (t[i] != i + 1) {
+                    out.println("0 0");
+                    return;
+                }
+            out.println((l + 1) + " " + (r + 1));
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public String nextLine() {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+    }
+}
