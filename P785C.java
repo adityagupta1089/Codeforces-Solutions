45d39f8fedd02b39ca4294e4c3b16015e17202d5
Aditya Gupta <adityagupta1089@gmail.com>
2017-03-21 20:21:20 +0530

    .

---
+++ P785C.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class P785C {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		Task solver = new Task();
+		solver.solve(in, out);
+		out.close();
+	}
+
+	static class Task {
+		public void solve(InputReader in, PrintWriter out) {
+			long n = in.nextLong();
+			long m = Math.min(n, in.nextLong());
+			long l=0;
+			long r = 1+(long) Math.sqrt(Long.MAX_VALUE);
+			while(l<r){
+				long mid = (l+r)/2;
+				long val = mid*(mid+1)/2;
+				if(val>=n-m){
+					r=mid;
+				}else{
+					l=mid+1;
+				}
+			}
+			System.out.println(m + l);
+		}
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+	}
+}
