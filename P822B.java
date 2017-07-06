a4b2402a86ea20455e538543f6c7b11fb9717715
Aditya Gupta <adityagupta1089@gmail.com>
2017-07-06 20:18:02 +0530

    .

---
+++ P822B.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class P822B {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		Task solver = new Task();
+		solver.solve(in, out);
+		out.close();
+	}
+
+	static class Task {
+		public void solve(InputReader in, PrintWriter out) {
+			int n = in.nextInt();
+			int m = in.nextInt();
+			String s = in.nextLine();
+			String t = in.nextLine();
+			List<Integer> p = new ArrayList<>();
+			int k = n + 1;
+			for (int i = 0; i + n <= m; i++) {
+				List<Integer> q = new ArrayList<>();
+				for (int j = i; j < i + n; j++) {
+					if (t.charAt(j) != s.charAt(j - i)) {
+						q.add(j - i + 1);
+					}
+				}
+				if (q.size() < k) {
+					k = q.size();
+					p = q;
+				}
+			}
+			out.println(p.size());
+			for (int q : p)
+				out.print(q + " ");
+			out.println();
+		}
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+	}
+}
