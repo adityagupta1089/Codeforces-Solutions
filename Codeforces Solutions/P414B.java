f572d5b95f76ce493e75d10d077dd6559ab9e408
Aditya Gupta <adityagupta1089@gmail.com>
2019-09-14 17:12:07 +0530

    Added P414B

--- Codeforces Solutions/P414B.java
+++ Codeforces Solutions/P414B.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class ${NAME} {
+public class P414B {
     public static void main(String[] args) {
         InputStream inputStream = System.in;
         OutputStream outputStream = System.out;
@@ -14,6 +14,31 @@ public class ${NAME} {
 
     static class Task {
         public void solve(InputReader in, PrintWriter out) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            long[][] dp = new long[n + 1][k + 1];
+            // dp[n][k] = number of good sequences of length k starting at n
+            // n
+            // dp[n][k] = dp[n][k-1] + dp[2n][k-1] + ... dp[xn][k-1]
+            // dp[j][i] = sum dp[l*j][i]
+            // n=3,k=2
+            for (int j = 1; j <= n; j++) {
+                dp[j][1] = 1;
+            }
+            for (int i = 2; i <= k; i++) {//i=2
+                for (int j = 1; j <= n; j++) {//j=1, dp[1][1]=dp[1][0]+dp[2][0]+...
+                    for (int l = 1; l * j <= n; l++) {
+                        dp[j][i] += dp[l * j][i - 1];
+                        dp[j][i] %= 1000000007;
+                    }
+                }
+            }
+            long sol = 0;
+            for (int i = 1; i <= n; i++) {
+                sol += dp[i][k];
+                sol %= 1000000007;
+            }
+            out.println(sol);
 
         }
     }
@@ -60,7 +85,7 @@ public class ${NAME} {
                 arr[i] = nextInt();
             return arr;
         }
-        
+
         public long[] nextLongArray(int n) {
             long[] arr = new long[n];
             for (int i = 0; i < n; i++)
@@ -69,5 +94,4 @@ public class ${NAME} {
         }
 
     }
-
 }
